a29c0b5bb29f3b5b581587282aed1a5632acc346
muchRandomness <118011747+muchRandomness@users.noreply.github.com>
2024-02-15 12:36:53 -0600

    Update Script.ts

--- Script.ts
+++ Script.ts
@@ -1,1 +1,9 @@
+let map : google.maps.Map;
+const center : google.maps.LatLngLiteral = {};
+function birdmap() {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center,
+    zoom: 8
+  });
 
+}
